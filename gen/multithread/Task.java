88107766d34eb4d5f17620842b3f9ecdc9e854e4
PhilipModDev <119460401+PhilipModDev@users.noreply.github.com>
2023-06-09 16:41:27 -0700

    Delete Task.java

--- gen/multithread/Task.java
+++
@@ -1,9 +0,0 @@
-package com.dawnfall.engine.gen.multithread;
-
-import java.util.concurrent.RecursiveAction;
-
-public interface Task<T extends RecursiveAction> {
-    void task();
-    void executeFork();
-    void executeInvoke();
-}
